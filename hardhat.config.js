46fbf40f92670bef1ba34a51c45efdeed845fe07
morfordkerry <111557677+morfordkerry@users.noreply.github.com>
2022-08-26 15:33:41 +0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,32 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    
+    "optimism": {
+       url: "https://opt-mainnet.g.alchemy.com/v2/jDSEEqFHv8Gecaf9dGKCgJLovDlUB5-w",
+       accounts: [ "0x9f00000000000000000000000000009099d92Bd3dd60d89" ]
+    }
+  }
+};
